40c487782b48d65f3473d1bfefc7f2c275afcf7a
Sibz <12050358+Sibz@users.noreply.github.com>
2020-09-06 18:46:10 +1000

    Moved star map deref code into own module

--- src/lib.rs
+++ src/lib.rs
@@ -1,31 +1,38 @@
+pub mod options;
+mod star_map_c;
 #[cfg(test)]
 mod tests;
 
-use std::slice;
-
-pub mod options;
 pub use options::StarMapOptions;
+use star_map_c::StarMapVecC;
 
 pub fn generate_star_map_in_preallocated_memory(
     options: StarMapOptions,
     pointer: *mut StarMapEntry,
 ) -> Result<(), String> {
+    let result = options.validate();
+
+    if result.is_err() {
+        return result;
+    }
+
     let mut x = StarMapVecC {
         ptr: pointer,
         len: options.object_count,
     };
+
     generate_into_slice(&mut x);
+
     Ok(())
-    //Err(String::from("re"))
 }
 
 pub fn generate(options: StarMapOptions) -> Vec<StarMapEntry> {
     let mut x: Vec<StarMapEntry> = vec![
         StarMapEntry {
-            x: 0,
-            y: 0,
-            z: 0,
-            w: 0,
+            x: 0f32,
+            y: 0f32,
+            z: 0f32,
+            w: 0f32,
         };
         options.object_count as usize
     ];
@@ -35,42 +42,23 @@ pub fn generate(options: StarMapOptions) -> Vec<StarMapEntry> {
 
 fn generate_into_slice(entries: &mut [StarMapEntry]) {
     entries.len();
-    let mut i: u32 = 0;
+    let mut i: f32 = 0f32;
     for e in entries.iter_mut() {
         e.x = i;
-        e.y = i + 1;
-        e.z = i + 2;
-        e.w = i + 3;
-        i = i + 1;
+        e.y = i + 1f32;
+        e.z = i + 2f32;
+        e.w = i + 3f32;
+        i = i + 1f32;
     }
 }
 
 #[repr(C)]
 #[derive(Clone)]
 pub struct StarMapEntry {
-    pub x: u32,
-    pub y: u32,
-    pub z: u32,
-    pub w: u32,
-}
-
-struct StarMapVecC {
-    ptr: *mut StarMapEntry,
-    len: u32,
-}
-
-impl std::ops::Deref for StarMapVecC {
-    type Target = [StarMapEntry];
-
-    fn deref(&self) -> &[StarMapEntry] {
-        unsafe { slice::from_raw_parts(self.ptr, self.len as usize) }
-    }
-}
-
-impl std::ops::DerefMut for StarMapVecC {
-    fn deref_mut(&mut self) -> &mut <Self as std::ops::Deref>::Target {
-        unsafe { slice::from_raw_parts_mut(self.ptr, self.len as usize) }
-    }
+    pub x: f32,
+    pub y: f32,
+    pub z: f32,
+    pub w: f32,
 }
 
 // impl Drop for CVec {
---
+++ src/star_map_c.rs
@@ -0,0 +1,21 @@
+use crate::StarMapEntry;
+use std::slice;
+
+pub struct StarMapVecC {
+    pub ptr: *mut StarMapEntry,
+    pub len: u32,
+}
+
+impl std::ops::Deref for StarMapVecC {
+    type Target = [StarMapEntry];
+
+    fn deref(&self) -> &[StarMapEntry] {
+        unsafe { slice::from_raw_parts(self.ptr, self.len as usize) }
+    }
+}
+
+impl std::ops::DerefMut for StarMapVecC {
+    fn deref_mut(&mut self) -> &mut <Self as std::ops::Deref>::Target {
+        unsafe { slice::from_raw_parts_mut(self.ptr, self.len as usize) }
+    }
+}
