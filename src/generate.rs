01c8fdf5d81eb2cbe61d6968fff5ef8249359c49
Sibz <12050358+Sibz@users.noreply.github.com>
2020-09-07 09:24:43 +1000

    added first impl of rotating around z

--- src/generate.rs
+++ src/generate.rs
@@ -1,5 +1,6 @@
 use crate::options::StarMapOptions;
 use crate::StarMapEntry;
+use interpolation::*;
 use rand::prelude::*;
 use rand::SeedableRng;
 use rand_pcg::Pcg64;
@@ -10,7 +11,11 @@ pub fn generate_into_slice(entries: &mut [StarMapEntry], options: StarMapOptions
     let mut pcg_rng = Pcg64::seed_from_u64(2);
 
     for e in entries.iter_mut() {
-        e.x = generate_x(i, i as f32 / len, options.core_size, &mut pcg_rng);
+        let x = generate_x(i, i as f32 / len, options.core_size, &mut pcg_rng);
+        let (x1, y1) = generate_xy(x, options.height, &mut pcg_rng);
+
+        e.x = x1;
+        e.y = y1;
         // e.y = i + 1f32;
         // e.z = i + 2f32;
         // e.w = i + 3f32;
@@ -36,4 +41,16 @@ fn generate_x(i: u32, percent_position: f32, core_size: f32, rng: &mut Pcg64) ->
             ),
         )
     }
+}
+
+fn generate_xy(x: f32, height: f32, rng: &mut Pcg64) -> (f32, f32) {
+    let angle = rng.gen_range(0f32, std::f32::consts::FRAC_PI_2);
+
+    let angle = lerp(&-angle, &angle, &(rng.gen_range(0f32, 1f32)));
+    rotate_2d(angle, x, 0f32)
+}
+
+fn rotate_2d(angle: f32, x: f32, y: f32) -> (f32, f32) {
+    let s_c = angle.sin_cos();
+    (x * s_c.1 - y * s_c.0, x * s_c.0 + y * s_c.1)
 }
